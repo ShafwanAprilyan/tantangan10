eb81c8be5116b43d622f3f6a58ea1cb75565176a
ShafwanAprilyan <49118098+ShafwanAprilyan@users.noreply.github.com>
2019-04-08 15:54:40 +0800

    Update BubbleSortDSC.cpp
    mengubah bubblesort ASC menjadi bubblesort DSC

--- BubbleSortDSC.cpp
+++ BubbleSortDSC.cpp
@@ -11,12 +11,12 @@ void swap(int *xp, int *yp)
 void bubbleSort(int arr[], int n)
 {
    int i, j;
-   for (i = 0; i < n-1; i++)
+   for (i = 0; i < n; i++)
 
      
-       for (j = 0; j < n-i-1; j++)
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
+       for (j = n-1; j > i; j--)
+           if (arr[j] > arr[j-1])
+              swap(&arr[j], &arr[j-1]);
 }
 
 void printArray(int arr[], int size)
